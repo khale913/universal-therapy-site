dff04bf9f811c87541bfc96eeac7e74f498aabaa
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-12-12 10:31:02 -0800

    home page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'resources', component: ResourcesComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
