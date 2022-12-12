59dabd9d87a512c6e5050da184f2b0599b91c2de
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-12-12 10:37:47 -0800

    basic navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostListener } from '@angular/core';
+import { ActivePageService } from 'src/app/service/active-page.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  activeTab: string = 'home';
+  navOpen: boolean = false;
+  hover: boolean = false;
+  count: any;
 
-  constructor() { }
+  @HostListener('document:scroll', ['$event'])
+  public onViewPortScroll() {
+
+
+
+
+  }
+
+  constructor(private tabService: ActivePageService, private router: Router) {
+    console.log(this.count);
+  }
 
   ngOnInit(): void {
+    this.tabService.count.subscribe(c => {
+      this.count = c;
+    })
+  }
+
+  navigateSection(section: string) {
+    this.activeTab = section;
+    document.getElementById(section)?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
+    this.router.navigate([section])
+    this.navOpen = false;
+  }
+
+  operateNav() {
+    console.log('open nav')
+    // document.getElementById("mySidenav").style.width = "250px";
+    this.navOpen = !this.navOpen;
   }
 
 }
--- src/app/service/active-page.service.ts
+++ src/app/service/active-page.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivePageService {
 
-  constructor() { }
+  counter: any = 1;
+  count: BehaviorSubject<number>;
+
+  constructor() {
+    this.count = new BehaviorSubject(this.counter);
+  }
+
+  nextCount(num: any) {
+    this.count.next(num);
+    // console.log(num)
+  }
 }
